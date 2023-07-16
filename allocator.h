ce86b8d167b021b0019a6dc45487066a2a5b6591
abincaps <abincaps@qq.com>
2023-07-17 00:20:50 +0800

    对象内容的构造和析构

--- allocator.h
+++ allocator.h
@@ -1,16 +1,17 @@
 #pragma once
 
-#include <iostream> // for cerr
-#include <cstddef>  // for ptrdiff_t, size_t
-#include <climits>  // for UINT_MAX
-#include <new>      // for placement new
-namespace cpp { 
+#include <climits>   // for UINT_MAX
+#include <cstddef>   // for ptrdiff_t, size_t
+#include <iostream>  // for cerr
+#include <new>       // for placement new
+namespace cpp {
 
 template <typename T1, typename T2>
 inline void construct(T1* pointer, const T2& arguments) {
-    // placement new操作符, 用于在已分配的内存上构造一个对象, 只负责调用对象的构造函数，不进行内存分配
-    // 将 T1 类型的对象构造在 pointer 所指向的已分配内存位置上，并向构造函数传递了 arguments
-    new(pointer) T1(arguments);
+    // placement new操作符, 用于在已分配的内存上构造一个对象,
+    // 只负责调用对象的构造函数，不进行内存分配 将 T1 类型的对象构造在 pointer
+    // 所指向的已分配内存位置上，并向构造函数传递了 arguments
+    new (pointer) T1(arguments);
 }
 
 template <typename T>
@@ -25,7 +26,6 @@ inline T* allocate(ptrdiff_t size, T*) {
     // 它返回的是一个指向已分配内存的 void* 指针，而不是指向对象的指针
     T* tmp = (T*)(::operator new(size_t(size * sizeof(T))));
 
-
     if (tmp == nullptr) {
         std::cerr << "out of memory" << std::endl;
         exit(1);
@@ -36,14 +36,15 @@ inline T* allocate(ptrdiff_t size, T*) {
 
 template <typename T>
 inline void deallocate(T* buffer) {
-    // ::operator delete 不会自动调用对象的析构函数。它仅用于释放内存，不会执行任何对象特定的清理操作
-    // delete 运算符在释放内存之前会自动调用对象的析构函数，以确保对象所占用的资源被正确释放
+    // ::operator delete
+    // 不会自动调用对象的析构函数。它仅用于释放内存，不会执行任何对象特定的清理操作 delete
+    // 运算符在释放内存之前会自动调用对象的析构函数，以确保对象所占用的资源被正确释放
     ::operator delete(buffer);
 }
 
 template <typename T>
 class allocator {
-public: 
+public:
     using value_type = T;
     using pointer = T*;
     using const_pointer = const T*;
@@ -51,30 +52,18 @@ public:
     using const_reference = const T&;
     using size_type = size_t;
     using difference_type = ptrdiff_t;
-    
-    pointer allocate(size_type n) {
-        return cpp::allocate(difference_type(n), pointer(nullptr));
-    }
 
-    void deallocate(pointer p) {
-        cpp::deallocate(p);
-    }
+    pointer allocate(size_type n) { return cpp::allocate(difference_type(n), pointer(nullptr)); }
 
-    pointer address(reference x) {
-        return pointer(&x);
-    }
+    void deallocate(pointer p) { cpp::deallocate(p); }
 
-    const_pointer address(const_reference x) {
-        return const_pointer(&x);
-    }
+    pointer address(reference x) { return pointer(&x); }
 
-    size_type init_page_size() {
-        return std::max(size_type(1), size_type(4096 / sizeof(T)));
-    }
+    const_pointer address(const_reference x) { return const_pointer(&x); }
 
-    size_type max_size() const {
-        return std::max(size_type(1), size_type(UINT_MAX / sizeof(T)));
-    }
-}; // end of class allocator
+    size_type init_page_size() { return std::max(size_type(1), size_type(4096 / sizeof(T))); }
 
-} // end of namespace cpp
+    size_type max_size() const { return std::max(size_type(1), size_type(UINT_MAX / sizeof(T))); }
+};  // end of class allocator
+
+}  // end of namespace cpp
---
+++ stl_construct.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <new>  // for placement new
+
+// <stl_construct>：定义了全局函数 construct() 和
+// destroy()，负责对象的构造和析构
+
+template <typename T1, typename T2>
+inline void construct(T1* pointer, const T2& arguments) {
+    // placement new操作符, 用于在已分配的内存上构造一个对象,
+    // 只负责调用对象的构造函数，不进行内存分配 将 T1 类型的对象构造在 pointer
+    // 所指向的已分配内存位置上，并向构造函数传递了 arguments
+    new (pointer) T1(arguments);
+}
+
+// 接受指针
+template <typename T>
+inline void destroy(T* pointer) {
+    // 调用析构函数释放
+    pointer->~T();
+}
+
+// ForwardIterator 单向序列中进行顺序遍历的迭代器
+// 接受两个迭代器, 调用 value_type() 获得迭代器所指对象的类别
+template <typename ForwardIterator>
+inline void destroy(ForwardIterator first, ForwardIterator last) {
+    __destroy(first, last, value_type(first));
+}
+
+// “trivial destructor”（平凡析构函数）
+// 没有显式定义析构函数；
+// 没有基类（或继承的基类具有平凡析构函数）；
+// 没有任何虚函数
+
+template <typename ForwardIterator, typename T>
+inline void __destroy(ForwardIterator first, ForwardIterator last, T*) {
+    // TODO: 等添加了__type_traits
+    using trivial_destructor = typename __type_traits<T>::has_trivial_destructor;
+    __destroy_aux(first, last, trivial_destructor());
+}
+
+// non-trivial destructor
+// 不是平凡析构函数
+template <typename ForwardIterator>
+inline void __destroy(ForwardIterator first, ForwardIterator last, __false_type) {
+    // 将 [first,last) 范围内的对象析构掉
+    for (; first < last; first++) {
+        // *first对迭代器first进行解引用, 得到它当前指向的对象
+        // &取这个对象的地址
+        // destory(first) 直接将迭代器first本身传递给destory函数
+        // 这意味着, 传递给destory的参数是一个迭代器, 不是一个对象指针。
+        destory(&*first);
+    }
+}
+
+// trivial destructor
+// 是平凡析构函数
+// 优化，什么也不做
+template <typename ForwardIterator>
+inline void __destroy(ForwardIterator first, ForwardIterator last, __true_type) {}
+
+// 泛型特化
+// inline void destroy(char*, char*) {}
+// inline void destroy(wchar_t*, wchar_t*) {}
